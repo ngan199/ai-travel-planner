982d53e5c4efe0c4b8d7a4cd4d6d97c461df3fd7
Crystal0997 <trankimngan190997@gmail.com>
2025-03-07 16:28:16 +0700

    buidl the header, home page content

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 function Header() {
   return (
-    <div>
-      Header
+    <div className='p-2 flex shadow-sm flex justify-between items-center'>
+      <img className='logo' src='/logo.svg' />
+      <div>
+        <Button>Sign in</Button>
+      </div>
     </div>
   )
 }
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 
 function index() {
+  console.log("import.meta.env.VITE_GOOGLE_PLACE_API_KEY}", import.meta.env.VITE_GOOGLE_PLACE_API_KEY)
   return (
-    <div>
-      create trip
+    <div className='flex flex-col items-center sm:px:10 md:px-12 lg:px-56 xl:px-10 px-5 mt-10'>
+      <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
+      <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, 
+      and our trip planner will generate a customized itinerary based on your preferences</p>
+
+      <div>
+        <div className='mt-20'>
+          <h2 className='text-xl my-3 font-medium'>What is your destination of choice?</h2>
+          <GooglePlacesAutocomplete 
+            apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+          />
+        </div>
+      </div>
     </div>
   )
 }
