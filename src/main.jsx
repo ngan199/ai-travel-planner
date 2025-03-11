7e5a2b9464422baecfbf04776eaf770602f2f641
Crystal0997 <trankimngan190997@gmail.com>
2025-03-11 20:02:34 +0700

    integrate gemini api

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import CreateTrip from './create-trip/index.jsx'
 import App from './App.jsx'
 import Header from './components/custom/Header'
+import { Toaster } from 'sonner'
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Header />
+    <Toaster />
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/create-trip" element={<CreateTrip />} />
